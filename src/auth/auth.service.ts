759db6bfbc42a7ab8cb72c21463f1adf2af851a1
9iero <86026337+9iero@users.noreply.github.com>
2025-05-04 22:42:30 -0400

    getting there

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { Prisma } from '@prisma/client'; // Add this line
 
 // service/provider is busy with business logic, while controller "controlls" requests
 
@@ -13,26 +14,26 @@ export class AuthService {
     
     async signup(dto: AuthDto) {
         //generate the password hash
-        const hash = await argon.hash(dto.password)
+        const hash = await argon.hash(dto.password);
 
         try{
 
-        //save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
-            select: {
-                id: true,
-                email: true,
-                createdAt: true,
-                updatedAt: true,
-            },
-        });
+            //save the new user in the db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+                select: {
+                    id: true,
+                    email: true,
+                    createdAt: true,
+                    updatedAt: true,
+                },
+            });
 
-        // return the saved user
-        return user;  
+            // return the saved user
+            return user;  
 
         } catch(error){
             if(error instanceof PrismaClientKnownRequestError){
@@ -45,15 +46,36 @@ export class AuthService {
 
     }
     
-    signin() {
-
+    async signin(dto: AuthDto) {
         // find user by email
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            } as Prisma.UserWhereUniqueInput, // Explicitly type the where clause
+            select: {
+                id: true,
+                hash: true,
+            },
+        });
+    
         // if user does not exist throw exception
-
+        if (!user)
+            throw new ForbiddenException(
+                'Credentials incorrect',
+            );
+    
         // compare password
+        const pwMatches = await argon.verify(
+            user.hash,
+            dto.password,
+        );
+    
         // if password incorrect throw exception
-
+        if (!pwMatches) {
+            throw new ForbiddenException('Credentials incorrect');
+        }
+    
         // send back the user
-        return { msg: 'i have signed in' }
+        return user;
     }
 }
