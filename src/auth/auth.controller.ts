6651034e26765445ff6421901f28a8cef8217419
9iero <86026337+9iero@users.noreply.github.com>
2025-05-04 20:00:40 -0400

    1/2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post} from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 // allows for controller to be clean, and only busy with logic for the requests, while service is busy w business logic
 
@@ -8,8 +9,8 @@ export class AuthController {
     constructor(private authService: AuthService){}
 
     @Post('signup')
-    signup() {
-        return this.authService.signup();
+    signup(@Body() dto: AuthDto) {
+        return this.authService.signup(dto);
     }
 
     @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,59 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 // service/provider is busy with business logic, while controller "controlls" requests
 
-@Injectable({})
+@Injectable()
 export class AuthService {
 
-    signin() {
-        return { msg: 'i have signed in' }
-    }
+    constructor(private prisma: PrismaService) {}
+    
+    async signup(dto: AuthDto) {
+        //generate the password hash
+        const hash = await argon.hash(dto.password)
 
-    signup() {
-        return { msg: 'i have signed up' }
+        try{
+
+        //save the new user in the db
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+            select: {
+                id: true,
+                email: true,
+                createdAt: true,
+                updatedAt: true,
+            },
+        });
+
+        // return the saved user
+        return user;  
+
+        } catch(error){
+            if(error instanceof PrismaClientKnownRequestError){
+                if(error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials Taken')
+                }
+            }
+            throw error;
+        }
+
+    }
+    
+    signin() {
+
+        // find user by email
+        // if user does not exist throw exception
+
+        // compare password
+        // if password incorrect throw exception
+
+        // send back the user
+        return { msg: 'i have signed in' }
     }
 }
